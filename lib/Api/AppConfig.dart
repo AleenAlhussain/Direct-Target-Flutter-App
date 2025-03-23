fa5f29441fb6004fa23cad3f05a2da6a08e77b37
AleenAlhussain <devaleenalhussain@gmail.com>
2025-03-23 18:12:06 +0300

    remove

--- lib/Api/AppConfig.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: file_names
-
-// String get appConfig {
-//   const appUrl = 'https://dowalyplus.aindubaico.com/api/v1';
-//
-//   return appUrl;
-// }
-const String appConfig = 'https://dowalyplus.aindubaico.com/api/v1';
-
